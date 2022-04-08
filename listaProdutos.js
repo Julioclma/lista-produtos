631982374a4b74c1a824150987210381862aa1cf
Julio Cesar <julioclmafra@gmail.com>
2022-04-08 02:17:18 -0300

    Sistema que lista produtos de acordo com a categoria escolhida

---
+++ listaProdutos.js
@@ -0,0 +1,78 @@
+function listProducts1() {
+
+    btn1 = document.querySelectorAll("#btn1")[0];
+
+
+
+    listagemProds = document.querySelectorAll(".container-produtos");
+
+
+    if (btn1) {
+
+
+        listagemProds[0].innerHTML = '<div class="produto"><div class="img-produto"><img src="camisetaPreta.jpg"></div><div class="descricao-produto"><p class="descricao-paragrafo">Camisa Preta - 24.99R$</p></div></div><div class="produto"><div class="img-produto"><img src="camisaMarrom.jpg"></div><div class="descricao-produto"><p class="descricao-paragrafo">Camisa Marrom - 30R$</p></div></div><div class="produto"><div class="img-produto"><img src="produto-img.png"></div><div class="descricao-produto"><p class="descricao-paragrafo">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p></div></div>'
+
+    }
+}
+
+function listProducts2() {
+
+    btn2 = document.querySelectorAll("#btn2")[0];
+
+
+    listagemProds = document.querySelectorAll(".container-produtos");
+
+    if (btn2) {
+
+
+        listagemProds[0].innerHTML = '<div class="produto"><div class="img-produto"><img src="produto-img.png"></div><div class="descricao-produto"><p class="descricao-paragrafo">Shorts ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p></div></div><div class="produto"><div class="img-produto"><img src="produto-img.png"></div><div class="descricao-produto"><p class="descricao-paragrafo">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p></div></div><div class="produto"><div class="img-produto"><img src="produto-img.png"></div><div class="descricao-produto"><p class="descricao-paragrafo">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p></div></div><div class="produto"><div class="img-produto"><img src="produto-img.png"></div><div class="descricao-produto"><p class="descricao-paragrafo">Shorts ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p></div></div>'
+
+    }
+}
+
+function listProducts3() {
+
+    btn3 = document.querySelectorAll("#btn3")[0];
+
+
+    listagemProds = document.querySelectorAll(".container-produtos");
+
+    if (btn3) {
+
+
+        listagemProds[0].innerHTML = '<div class="produto"><div class="img-produto"><img src="produto-img.png"></div><div class="descricao-produto"><p class="descricao-paragrafo">Sapatos ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p></div></div><div class="produto"><div class="img-produto"><img src="produto-img.png"></div><div class="descricao-produto"><p class="descricao-paragrafo">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p></div></div>'
+
+    }
+}
+
+function listProducts4() {
+
+    btn2 = document.querySelectorAll("#btn4")[0];
+
+
+    listagemProds = document.querySelectorAll(".container-produtos");
+
+    if (btn4) {
+
+
+        listagemProds[0].innerHTML = '<div class="produto"><div class="img-produto"><img src="produto-img.png"></div><div class="descricao-produto"><p class="descricao-paragrafo">Mangas ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p></div></div><div class="produto"><div class="img-produto"><img src="produto-img.png"></div><div class="descricao-produto"><p class="descricao-paragrafo">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p></div></div><div class="produto"><div class="img-produto"><img src="produto-img.png"></div><div class="descricao-produto"><p class="descricao-paragrafo">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p></div></div><div class="produto"><div class="img-produto"><img src="produto-img.png"></div><div class="descricao-produto"><p class="descricao-paragrafo">Shorts ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p></div></div>'
+
+    }
+}
+
+function listProducts5() {
+
+    btn5 = document.querySelectorAll("#btn5")[0];
+
+
+    listagemProds = document.querySelectorAll(".container-produtos");
+
+    if (btn5) {
+
+
+        listagemProds[0].innerHTML = '<div class="produto"><div class="img-produto"><img src="produto-img.png"></div><div class="descricao-produto"><p class="descricao-paragrafo">Calças ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p></div></div><div class="produto"><div class="img-produto"><img src="produto-img.png"></div><div class="descricao-produto"><p class="descricao-paragrafo">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p></div></div><div class="produto"><div class="img-produto"><img src="produto-img.png"></div><div class="descricao-produto"><p class="descricao-paragrafo">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p></div></div>'
+
+    }
+
+}
+
